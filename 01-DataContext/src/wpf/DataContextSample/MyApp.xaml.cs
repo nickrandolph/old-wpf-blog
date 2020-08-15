f388ddaebf93347a68a8a85a938a53522833e970
Nick Randolph <nick@builttoroam.com>
2020-08-15 16:14:39 +1000

    Updating 01 to include wpf on .net core and .net 5

--- 01-DataContext/src/wpf/DataContextSample/MyApp.xaml.cs
+++ 01-DataContext/src/wpf/DataContextSample/MyApp.xaml.cs
@@ -1,20 +1,8 @@
-using System;
 using System.Windows;
-using System.Data;
-using System.Xml;
-using System.Configuration;
 
 namespace DataContextSample
 {
-    /// <summary>
-    /// Interaction logic for MyApp.xaml
-    /// </summary>
     public partial class MyApp : Application
     {
-        private void AppStartingUp(object sender, StartupEventArgs e)
-        {
-            Window1 mainWindow = new Window1();
-            mainWindow.Show();
-        }
     }
 }
--- 01-DataContext/src/wpf/DataContextSample/Window1.xaml.cs
+++ 01-DataContext/src/wpf/DataContextSample/Window1.xaml.cs
@@ -1,10 +1,4 @@
-using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Media;
-using System.Windows.Shapes;
 
 
 namespace DataContextSample
